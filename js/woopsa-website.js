f0e5b86a4448ae17250a03038a11cf91e58ef7e8
floriansegginger <f@floriansegginger.ch>
2015-12-09 13:32:41 +0100

    Multiple code examples on homepage

--- js/woopsa-website.js
+++ js/woopsa-website.js
@@ -5,4 +5,22 @@ $(document).ready(function (){
 			$(".how-it-works .image .response").addClass("visible");
 		}, 500);
 	});
+	
+	$(".client-side").cycle({
+		timeout: (Math.random()*2000+10000),
+		speed: 200
+	});
+	
+	$(".server-side").cycle({
+		timeout: (Math.random()*2000+10000),
+		speed: 200
+	});
+	
+	$(".code-sample-navigate-previous").click(function (){
+		$(this).parents(".code-sample-side").cycle("prev");
+	});
+	
+	$(".code-sample-navigate-next").click(function (){
+		$(this).parents(".code-sample-side").cycle("next");
+	});
 });
